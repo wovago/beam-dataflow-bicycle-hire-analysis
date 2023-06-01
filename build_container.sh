94850a3e59e2499f9eec83bf57db8df992e8a2ce
wovago <9787439+wovago@users.noreply.github.com>
2023-06-01 16:09:15 +0200

    add container build script

---
+++ build_container.sh
@@ -0,0 +1,14 @@
+source ./environment.sh
+
+# Create container repository
+gcloud artifacts repositories create $REPOSITORY \
+    --repository-format=docker \
+    --location=$REGION \
+    --async
+
+# Add gcloud as Docker credential helper
+gcloud auth configure-docker $REGION-docker.pkg.dev
+
+# Build container image using Google cloud build and push to repository
+gcloud builds submit --tag $REGION-docker.pkg.dev/$PROJECT/$REPOSITORY/dataflow/beam:2.47.0 .
+
